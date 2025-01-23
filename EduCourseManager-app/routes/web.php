dc6de831f6d0f7ca11a6c42a189fbdc224b712b6
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-01-23 22:45:46 +0700

    Feat: Authentikasi

--- EduCourseManager-app/routes/web.php
+++ EduCourseManager-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
